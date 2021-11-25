e8c6620fd425df617f6ce5071e27e0cf0ccbfa24
blekenbleu <blekenbleu@gmail.com>
2021-11-25 09:28:16 -0500

    only gamepad enabled

--- midi_gamepad/midi_gamepad.ino
+++ midi_gamepad/midi_gamepad.ino
@@ -1,38 +1,53 @@
 /*
- ; Sample MIDI sketch
- ; author: chegewara
+ ; Sample MIDI + gamepad sketch
+ ; author: blekenbleu
  */
+
+#if 0			// MIDI
+#define DM 1
 #include "midiusb.h"
 #include "song.h"
-#if 1
+#endif
+
+#if 1			// gamepad
 #define DG 1
 #include "hidgamepad.h"
 
 HIDgamepad gamepad;
 #endif
 
+#ifdef DM
 MIDIusb midi;
+#endif
 
 void setup() {
   Serial.begin(115200);
-  char name[] = "midi.ino";
+
+#ifdef DM
+  char name[] = "midi_gamepad.ino";
   midi.begin(name);
+  Serial.write("MIDIusb begun\n");
+#endif
+
 #ifdef DG
   gamepad.begin();
   Serial.write("HIDgamepad begun\n");
 #endif
+
+#ifdef DM
+  Serial.write("delay(1000)");  
   delay(1000);
-#if 0
   if(0 && midi.setSong(song, song_len)) {
     Serial.write("midi.setSong() true\n");
     midi.playSong();
     Serial.write("midi.playSong() ended\n");
   } else Serial.write("midi.setSong() false\n");
-#else
-  Serial.write("MIDIusb begun\n");
 #endif
+
+  Serial.write("setup() complete\n");
 }
 
+#ifdef DM
 // Sample melody as an array of note values
 uint8_t note_sequence[] =
 {
@@ -55,19 +70,23 @@ void midi_task(void)
   // Send Note On for current position at full velocity (127) on channel 1.
   midi.noteON(note_sequence[note_pos++], 127);
 }
+#endif
 
 #ifdef DG
 void gamepad_task()
 {
   static uint8_t i = 255;
-  uint32_t buttons = i;
+  static uint32_t count = 0;
+  static bool ping = true;
 
-  buttons |= (buttons << 8);
-  buttons |= (buttons << 16);
-  gamepad.sendAll(buttons, i, i, i, i, i, i, i%10);
+  if (ping)  // 32 buttons, 3 axes, 3 rotations, 8 hat positions
+    gamepad.sendAll(1 << (i%32), i, i, i, i, i, i, 1 + i%8);
+  else       // axes and rotations are actually 8-bit signed
+    gamepad.sendAll(1 << (31 - i%32),255 - i, i, 255 -i, i, 255 - i, i, 8 - i%8);
   if (255 <= i) {
     i = 0;
-    Serial.write("gamepad_task() loop\n");
+    ping = !ping;
+    Serial.printf("gamepad_task() %ld loops\n", 256 * count++);
   }
   else i++;
 }
@@ -80,7 +99,9 @@ void loop() {
   if (millis() - start_ms >= 286) {
     start_ms += 286;
 
+#ifdef DM
     midi_task();
+#endif
 #ifdef DG
     gamepad_task();
 #endif
